dd4d70a43d085fb6e363715290047583a4d04419
Olivier Smith <oliviersmith@Oliviers-MacBook-Pro.local>
2025-06-30 13:02:23 +0100

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/consentia">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
